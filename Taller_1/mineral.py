37dfbc4767550662fffdd359229d84b93d3435fb
gperdomoq <g.perdomoq@uniandes.edu.co>
2023-09-07 23:47:25 -0500

    2.1 y 2.2

--- Taller_1/mineral.py
+++ Taller_1/mineral.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 class Mineral:
     def __init__(self, nombre, dureza, lustre, rompimiento_fractura, color, composicion, sistema_cristalino, specific_gravity):
         self.nombre = nombre
@@ -18,3 +20,25 @@ class Mineral:
                f"Composición: {self.composicion}\n" \
                f"Sistema cristalino: {self.sistema_cristalino}\n" \
                f"Specific Gravity: {self.specific_gravity}"
+              
+    def es_silicato(self):
+        # Verifica si "Si" y "O" están presentes en la composición química
+        return "Si" in self.composicion and "O" in self.composicion
+
+    def densidad_si(self):
+        # Calcula la densidad en unidades SI (kg/m^3)
+        return self.specific_gravity * 1000
+
+    def visualizar_color(self):
+        # Visualiza el color del mineral más común usando matplotlib
+        plt.figure()
+        plt.title(f"Color del Mineral: {self.nombre}")
+        plt.imshow([[self.color]])
+        plt.axis('off')
+        plt.show()
+
+    def imprimir_informacion(self):
+        # Imprime la información de dureza, tipo de rompimiento y sistema de organización de átomos
+        print(f"Dureza: {self.dureza}")
+        print(f"Tipo de Rompimiento: {'Fractura' if self.rompimiento_fractura else 'Escisión'}")
+        print(f"Sistema de Organización de Átomos: {self.sistema_cristalino}")
